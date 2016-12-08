86dd7c67ffaa1927f2deb1b7900458760ae77583
Elmar Sonnenschein <eso@esoco.de>
2016-12-08 11:32:29 +0100

    BF: wrong updateValue() invocation sequence

--- src/main/java/de/esoco/gwt/client/ui/ValueListDataElementUI.java
+++ src/main/java/de/esoco/gwt/client/ui/ValueListDataElementUI.java
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	  http://www.apache.org/licenses/LICENSE-2.0
+//		 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -167,8 +167,6 @@ public class ValueListDataElementUI extends DataElementUI<DataElement<?>>
 	@Override
 	protected void updateValue()
 	{
-		super.updateValue();
-
 		Component	   rComponent   = getElementComponent();
 		DataElement<?> rDataElement = getDataElement();
 		List<String>   rValues	    =
@@ -191,6 +189,8 @@ public class ValueListDataElementUI extends DataElementUI<DataElement<?>>
 		{
 			updateButtons(rValues, bAllowedValuesChanged);
 		}
+
+		super.updateValue();
 	}
 
 	/***************************************
